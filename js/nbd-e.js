8d6ecbc0af09bb2731d15af82476bea8b5e0748b
lifeinpoetry <gabrielle@frosti.org>
2017-08-08 06:32:42 -0700

    Update and rename nbd-d.js to nbd-e.js

--- js/nbd-e.js
+++ js/nbd-e.js
@@ -73,7 +73,7 @@
 
             $("title").text( completeJsHeadline );
 
-            $(".blog-title").replaceWith($("<h2 class='blog-title'>" + $("h1.source").text() + "</h2>"));
+            $(".blog-title").replaceWith($("<h2 class='blog-title'>" + $(".blog-title").text() + "</h2>"));
         }
     } else {
         if ( $("article.text").length  )
@@ -113,7 +113,7 @@
                         $("title").text( completeJsHeadline );
                     }
 
-                    $(".blog-title").replaceWith($("<h2 class='blog-title'>" + headElement.html() + "</h2>"));
+                    $(".blog-title").replaceWith($("<h2 class='blog-title'>" + $(".blog-title").text() + "</h2>"));
                 }
             }
         }
