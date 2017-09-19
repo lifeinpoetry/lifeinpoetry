2e1df43d5490d1bd97bd47d8c15042f609fabd3a
lifeinpoetry <gabrielle@frosti.org>
2017-09-19 06:07:05 -0700

    Update and rename nbd-q.js to nbd-r.js

--- js/nbd-r.js
+++ js/nbd-r.js
@@ -273,6 +273,7 @@ WebFont.load({
      $( ".nav-menu > ul > li > a.open:not( .selected )" ).click(function(e) {
         e.preventDefault();
         $(".pop-menu:visible").hide(150);
+        $( ".pop-menu" ).removeClass( "pop-add" );
         $( this ).addClass( "selected" );
         $( this ).parents("li").children("ul").show( "slow", function() {
                 $( this ).addClass( "pop-add" );
