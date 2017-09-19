a93043b2f470031d75469f883d7b656f9634d320
lifeinpoetry <gabrielle@frosti.org>
2017-09-19 07:35:18 -0700

    Update and rename nbd-zq.js to nbd-zr.js

--- js/nbd-zr.js
+++ js/nbd-zr.js
@@ -326,11 +326,11 @@ WebFont.load({
         newscroll = mywindow.scrollTop();
 
         if (newscroll > mypos && ( ( newscroll - mypos) > 10 ) && !up) {
-            $(".pop-menu:visible").stop().slideUp();
+            $(".pop-menu:visible").removeClass("pop-add").stop().slideUp();
             $(".share-menu:visible").stop().slideToggle();
             up = !up;
         } else if(newscroll < mypos && ( ( mypos - newscroll ) > 10 ) && up) {
-            $(".pop-menu:visible").stop().slideUp();
+            $(".pop-menu:visible").removeClass("pop-add").stop().slideUp();
             $(".share-menu:visible").stop().slideToggle();
             up = !up;
         }
